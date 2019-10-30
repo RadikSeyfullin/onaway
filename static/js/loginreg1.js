15c34f0a3846862c9ae75370f639c2a13f1831a4
RadikSeyfullin <radikseyfullin@gmail.com>
2019-10-31 00:11:10 +0300

    Alpha v0.4.5

--- static/js/loginreg1.js
+++
@@ -1,7 +0,0 @@
- /*$('.reg').click(function() {*/
-    $('.reg').css("font-size", "50px");
-    $('.left').css("font-size", "30px");
-    $('.regform').css('display','block');
-    $('.loginform').hide();
-    /*$('.regform').css('display','block');
-    $('.loginform').css('display','none');*/
